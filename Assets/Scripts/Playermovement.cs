892108870318fcca0343538f51f6099ac9d1df74
daniboy4ever <0218208@up.edu.mx>
2025-05-17 11:41:34 -0600

    game
    actualizacion de movimiento solo en x

--- Assets/Scripts/Playermovement.cs
+++ Assets/Scripts/Playermovement.cs
@@ -4,44 +4,31 @@ public class Playermovement : MonoBehaviour
 {
     public float velocidad = 5f;
 
-    
+    private Vector2 movement;
+
+    private Rigidbody rb;
+
+    //Delimitar borde
     public float xMin = -8f;
     public float xMax = 8f;
     public float yMin = -4.5f;
     public float yMax = 4.5f;
 
+
+    void Start()
+    {
+        //rb = GetComponent<Rigidbody2D>();
+    }
+
     void Update()
     {
-        float moveX = Input.GetAxis("Horizontal");
-        float moveY = Input.GetAxis("Vertical");
+        //Moviemiento del jugador
+        movement.x = Input.GetAxisRaw("Horizontal");
+        transform.Translate(movement * velocidad * Time.deltaTime);
 
-        Vector3 movement = new Vector3(moveX, moveY, 0f) * velocidad * Time.deltaTime;
-        transform.position += movement;
-
-    
+        //Bordes
         float clampedX = Mathf.Clamp(transform.position.x, xMin, xMax);//Mathf.Clamp, que es una función que limita un valor dentro de un rango.
-
-
-        float clampedY = Mathf.Clamp(transform.position.y, yMin, yMax);
-
-        transform.position = new Vector3(clampedX, clampedY, transform.position.z);
+        transform.position = new Vector2(clampedX, transform.position.y);
     }
 }
-
-
-/*using UnityEngine;
-
-public class Playermovement : MonoBehaviour
-{
-    public float velocidad = 5f;
-
-    void Update()
-    {
-        float moveX = Input.GetAxis("Horizontal");
-        float moveY = Input.GetAxis("Vertical");
-
-        Vector3 movement = new Vector3(moveX, moveY, 0f);
-        transform.position += movement * velocidad * Time.deltaTime;
-    }
-}*/
 
